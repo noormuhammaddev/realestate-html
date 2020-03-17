67942a622cbba25b038ed82c60f09d0bf99fcbeb
Noor <links2design@gmail.com>
2020-03-17 23:27:34 +0500

    add footer html

--- footer.php
+++ footer.php
@@ -1,1 +1,30 @@
-hera
+<footer>
+	<div class="countanir">
+		<div class="row">
+			<div class="col-md-4">
+				<h3>Company</h3>
+					<ul>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Advertisign</a></li>
+						<li><a href="#">careers</a></li>
+						<li><a href="#">terms off use</a></li>
+						<li><a href="#">privacy policy</a></li>
+						<li><a href="#">Blog</a></li>
+					</ul>
+			</div>
+			<div class="col-md-4">
+				<h3>Contactus</h3>
+					<ul>
+						<li>About us</li>
+						<li><a href="mailto: info@graana.com">Mail:info@graana.com</a></li>
+						<li><a href="tell: 123456789">Uan:123456789</a></li>
+					</ul>
+			</div>
+			<div class="col-md-4">
+				<h3>Newsletter</h3>
+			</div>
+		</div>
+	</div>
+
+
+</footer>
