39c6bb7cdbe1fbc52eaa33589d42b2ba0464964a
Noor <links2design@gmail.com>
2020-03-18 23:32:44 +0500

    footer complet

--- footer.php
+++ footer.php
@@ -14,15 +14,33 @@
 			</div>
 			<div class="col-md-4">
 				<h3>Contactus</h3>
-					<ul>
-						<li>About us</li>
-						<li><a href="mailto: info@graana.com">Mail:info@graana.com</a></li>
-						<li><a href="tell: 123456789">Uan:123456789</a></li>
+					<ul class="contact-info">
+						<li class="address">Address: 4th Floor, Beverly Centre, Blue Area, Dubai</li>
+						<li class="email"><a href="mailto: info@graana.com">Mail:info@graana.com</a></li>
+						<li class="phone"><a href="tell: 123456789">Uan:123456789</a></li>
 					</ul>
 			</div>
 			<div class="col-md-4">
 				<h3>Newsletter</h3>
+				<form>
+					<div class="newsleter-field">
+						<input type="email" class="form-control">
+						<button type="button" class="btn btn-warning">Subscribe
+					</div>	
+				</form>
+				<h3>Follow Us</h3>
+					<div class="social-media">
+						<a href="#" class="fb"></a>
+						<a href="#" class="insta"></a>
+						<a href="#" class="twitter"></a>
+						<a href="#" class="gplus"></a>
+					</div>
 			</div>
+		</div>
+
+		<!-- copy right text -->
+		<div class="copy-right">
+		© 2020 abc all right reserved.
 		</div>
 	</div>
 </footer>
