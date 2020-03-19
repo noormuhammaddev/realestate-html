9be1ae3a617051d95be4b4a43cc2ae51e47cbda9
Noor Muhammad <noor.muhammad@keeptruckin.com>
2020-03-19 22:28:18 +0500

    add small objects

--- contact.php
+++ contact.php
@@ -9,9 +9,14 @@
 
   <!-- add header -->
     <?php include 'header.php'; ?>
+    
+    <!-- small objects -->
+    <span class="object orange"></span>
+    <span class="object blue"></span>
 
   <!-- contact page content -->
     <section class="contact">
+        
         <div class=container>
             <h1>Contact Us</h1>
             <h2>How can we help you?</h2>
@@ -19,12 +24,12 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input type="text" class="form-control form-control-lg" placeholder="Enter your name">
+                            <input type="text" class="form-control" placeholder="Enter your name">
                         </div>  
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input type="text" class="form-control form-control-lg" placeholder="Enter your email">
+                            <input type="text" class="form-control" placeholder="Enter your email">
                         </div>
                     </div>
                 </div>
@@ -32,31 +37,31 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input type="text" class="form-control form-control-lg" placeholder="Enter your phone number">
+                            <input type="text" class="form-control" placeholder="Enter your phone number">
                         </div> 
                     </div>
 
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input type="text" class="form-control form-control-lg" placeholder="Enter your subject">
+                            <input type="text" class="form-control" placeholder="Enter your subject">
                         </div> 
                     </div>
                 </div> 
 
                 <div class="row">
                     <div class="col-md-12">
-                        <textarea class="form-control form-control-lg" placeholder="Enter your message"></textarea>
-                        <button type="submit" class="btn btn-warning">Send Message</button>
+                        <textarea class="form-control" placeholder="Enter your message"></textarea>
                     </div>
                 </div>
                 
+                <button type="submit" class="btn btn-warning btn-lg search-btn cta">Send Message</button>
 
             </form>
 
             <div class="contact-info-container">
                 <div class="row">
                     <div class="col-md-7">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.5450300315465!2d72.99617706454374!3d33.66894883071183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df95ff25ef0ae9%3A0xd74faf85d90645e5!2sG-11%20Markaz%20G%2011%20Markaz%20G-11%2C%20Islamabad%2C%20Islamabad%20Capital%20Territory%2C%20Pakistan!5e0!3m2!1sen!2s!4v1584561311726!5m2!1sen!2s" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.5450300315465!2d72.99617706454374!3d33.66894883071183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df95ff25ef0ae9%3A0xd74faf85d90645e5!2sG-11%20Markaz%20G%2011%20Markaz%20G-11%2C%20Islamabad%2C%20Islamabad%20Capital%20Territory%2C%20Pakistan!5e0!3m2!1sen!2s!4v1584561311726!5m2!1sen!2s" width="100%" height="210" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                     </div>  
                     <div class="col-md-5">
                         <ul class="contact-info">
